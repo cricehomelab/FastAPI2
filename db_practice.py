36b434f587c2defb6c8f16a2e21d7e2ebb1bafc7
cricehomelab <charles.e.riceiii@gmail.com>
2023-01-03 18:36:43 -0500

    learning sqlalchemy

---
+++ db_practice.py
@@ -0,0 +1,80 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text
+import logging
+
+# logging config setup.
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+
+# testing the logging.
+# https://docs.sqlalchemy.org/en/14/tutorial/index.html#unified-tutorial
+logging.info(f'DB Module starting....')
+logging.info(f'SqlAlchemy version: {sqlalchemy.__version__}')
+
+
+
+# https://docs.sqlalchemy.org/en/14/tutorial/engine.html
+
+# Memory based DB creation.
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+
+# https://docs.sqlalchemy.org/en/14/tutorial/dbapi_transactions.html
+
+'''
+# Does not commit to DB. 
+with engine.connect() as conn:
+    results = conn.execute(text("select 'hello world'"))
+    logging.info(f'number of results in query: {len(results.all())}')
+    for result in results.all():
+        logging.info(result)
+'''
+
+# Commiting to to the DB. 
+sql_create_table = "CREATE TABLE some_table (x int, y int)"
+sql_insert_command = "INSERT INTO some_table (x, y) VALUES (:x, :y)"
+
+'''
+with engine.connect() as conn:
+    conn.execute(text(sql_create_table))
+    conn.execute(text(sql_insert_command), [{"x": 1, "y": 1}, {"x": 2, "y": 4}],)
+    conn.commit()
+'''
+
+list = [
+    {"x": 1, "y": 1},
+    {"x": 2, "y": 4},
+    {"x": 3, "y": 7},
+    {"x": 4, "y": 10},
+    {"x": 5, "y": 13},
+    {"x": 6, "y": 16},
+    {"x": 7, "y": 19},
+    {"x": 8, "y": 22},
+    {"x": 9, "y": 25},
+    {"x": 10, "y": 28}
+    ]
+
+with engine.connect() as conn:
+    conn.execute(text(sql_create_table)) # Creates the table in sql.
+    conn.execute(text(sql_insert_command), list) # commits values to DB. 
+    conn.commit()
+
+# Basic query.
+sql_query_command = "SELECT x, y FROM some_table"
+
+with engine.connect() as conn:
+    results = conn.execute(text(sql_query_command)) # Running query on commited data. 
+    for result in results:
+        logging.info(result)
+
+sql_query_command = """
+                    SELECT
+                        x,
+                        y
+                    FROM
+                        some_table
+                    WHERE
+                        y > :y"""
+
+with engine.connect() as conn:
+    results = conn.execute(text(sql_query_command), {"y": 10})
+    for result in results:
+        logging.info(result)
